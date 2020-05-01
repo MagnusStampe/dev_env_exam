0ba6934bd317978bae0bb6ea3640ce16aeaf54a5
Magnus stampe Nielsen <m_s_nielsen288@hotmail.com>
2020-05-01 14:26:17 +0200

    Initial setup of Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+// Components
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/"
+            component={props => <Home {...props} />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/forms/InputTag.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+
+// Styles
+import styles from './InputTag.module.css'
+
+export default class InputTag extends Component {
+    state = {
+        inputID: 'input-' + Math.floor(Math.random() * 100000)
+    }
+
+    render() {
+        const {
+            props: {
+                label,
+                name,
+                name2,
+                type
+            },
+            state: {
+                inputID
+            }
+        } = this
+
+        const containerClasses = classnames(
+            name2 && styles.doubleInput,
+            styles.inputContainer
+        )
+
+        return (
+            <div className={containerClasses}>
+                <label className={styles.label} htmlFor={inputID}>{label}</label>
+                <input className={styles.input} type={type} id={inputID} name={name} />
+                {name2 && (
+                    <input className={styles.input} type={type} name={name2} />
+                )}
+            </div>
+        )
+    }
+}
+
+export class TextareaTag extends Component {
+    state = {
+        textareaID: 'textarea-' + Math.floor(Math.random() * 100000)
+    }
+    render() {
+        const {
+            props: {
+                label,
+                name
+            },
+            state: {
+                textareaID
+            }
+        } = this
+
+        const containerClasses = classnames(
+            styles.inputContainer,
+            styles.tall
+        )
+
+        return (
+            <div className={containerClasses}>
+                <label htmlFor={textareaID}>{label}</label>
+                <textarea className={styles.input} id={textareaID} name={name} />
+            </div>
+        )
+    }
+}
