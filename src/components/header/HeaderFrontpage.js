2f23c701f5b3d93e5504a8879b1231f3d895cfda
EliasLip <elia0649@kea.stud.com>
2020-05-15 11:12:07 +0200

    header - under progress

---
+++ src/components/header/HeaderFrontpage.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+export default class HeaderMain extends Component {
+  state = {
+    destination: null,
+    checkIn: null,
+    checkOut: null,
+    guests: null
+  };
+
+  render() {
+    console.log("save and test: " + this.state.destination);
+    console.log(this.props);
+    return (
+      <div>
+        <div className="flexWrapper">
+          <div className="logoContainer">
+            <div className="logo">ddddddd</div>
+          </div>
+
+          <div className="form">
+            <form
+              onSubmit={() => this.props.handleTravelInfoChange(this.state)}
+            >
+              <input
+                type="text"
+                className="destination"
+                onChange={event =>
+                  this.setState({ destination: event.target.value })}
+              />
+              <input
+                type="date"
+                className="checkIn"
+                onChange={event =>
+                  this.setState({ checkIn: event.target.value })}
+              />
+              <input
+                type="date"
+                className="checkOut"
+                onChange={event =>
+                  this.setState({ checkOut: event.target.value })}
+              />
+              <input
+                type="number"
+                className="guests"
+                onChange={event =>
+                  this.setState({ guests: event.target.value })}
+              />
+              <button>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
