86fe8e4f14f57e5f95ee4c9f8d37b0f7e39c7736
Magnus stampe Nielsen <m_s_nielsen288@hotmail.com>
2020-05-29 15:21:35 +0200

    create user with node-postres

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,13 @@ const router = require("express").Router()
 
 const dbCredentials = require('./../config/dbcredentials')
 const { Client } = require('pg')
+const client = new Client({
+    user: dbCredentials.user,
+    host: dbCredentials.host,
+    database: dbCredentials.database,
+    password: dbCredentials.password,
+    port: dbCredentials.port,
+})
 
 //##    GET     ##
 // User session
@@ -9,48 +16,49 @@ const { Client } = require('pg')
 //##    POST    ##
 // Create user
 router.post('/users/create', (req,res) => {
-    if(!req.query) {
-        return res.status(404).send({status: 0, message: 'No parameters provided'})
-    }
-    const { 
-        username, 
-        password, 
-        email,
-        name, 
-        phonenumber: phoneNumber, 
-        phonecode: phoneCode, 
-        countrycode: countryCode, 
-        iban: IBAN, 
-        ccv: CCV 
-    } = req.headers
+    if(!req.query) return res.status(404).send({status: 0, message: 'No parameters provided'})
     
+    const { 
+        username,
+        password,
+        email,
+        name,
+        phonenumber: phoneNumber,
+        phonecode: phoneCode,
+        countrycode: countryCode,
+        iban: IBAN,
+        ccv: CCV,
+        expirationdate: expirationDate
+    } = req.headers
+
     if(
-        !username 
-        || !password 
-        || !email 
-        || !name 
-        || !phoneNumber 
-        || !phoneCode 
-        || !countryCode 
-        || !IBAN 
+        !username
+        || !password
+        || !email
+        || !name
+        || !phoneNumber
+        || !phoneCode
+        || !countryCode
+        || !IBAN
         || !CCV
-    ) {
-        return res.status(404).send({status: 0, message: 'Insufficient parameters provided'})
-    }
+        || !expirationDate
+    ) return res.status(404).send({status: 0, message: 'Insufficient parameters provided'})
 
-    const client = new Client({
-        user: dbCredentials.user,
-        host: dbCredentials.host,
-        database: dbCredentials.database,
-        password: dbCredentials.password,
-        port: dbCredentials.port,
-    })
+    const query = `CALL proUserInsert(
+        '${username}',
+        '${password}',
+        '${email}',
+        '${phoneNumber}',
+        '${phoneCode}',
+        '${countryCode}',
+        '${IBAN}',
+        '${CCV}',
+        '${expirationDate}'
+    );`
 
-    //const query = `EXEC prouserinsert("${username}", "${password}", "${email}", "${phoneNumber}")`
-    const query = 'SELECT * FROM public."tUser"'
     client.connect()
     client.query(query, (err, dbRes) => {
-        if(err)  return res.status(500).send({status: 0, message: 'Error'})
+        if(err) return res.status(500).send({status: 0, message: 'Error'})
 
         client.end()
         return res.status(200).send({status: 1, message: 'User created'})
@@ -64,9 +72,6 @@ router.post('/users/create', (req,res) => {
 //##    DELETE  ##
 // Delete user
 router.delete('/users/delete/:username', (req,res)=>{
-    if(res.session.username !== 'b') {
-        res.status(404).send({message: 'Unauthorized delete'})
-    }
     try {
         //Users.delete()
         res.status(200).send({message: 'Succes'})
